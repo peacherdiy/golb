88728cf95e04706028fc95b8df627521c1bfbb39
he sheng <peacherdiy@126.com>
2012-12-28 17:49:49 +0800

    update ignore

--- lovelearning/src/main/java/org/golb/lovelearning/repository/UserDao.java
+++
@@ -1,8 +0,0 @@
-package org.golb.lovelearning.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.golb.lovelearning.entity.User;
-
-public interface UserDao extends PagingAndSortingRepository<User, Long> {
-	User findByLoginName(String loginName);
-}
--- lovelearning/target/test-classes/data/cleanup-data.sql
+++
@@ -1,2 +0,0 @@
-delete from lltask;
-delete from lluser;
