eaddae65d773605e17201a5e03704fc33802950f
peacherdiy <peacherdiy@126.com>
2013-03-29 15:56:55 +0800

    add new idea

--- lovelearning/product/killProcess/killProcess.bat
+++
@@ -1,8 +0,0 @@
-echo 请输入杀掉的端口:
-set /p portNumber=
-echo "端口号 %portNumber%"
-netstat -ano | find "%portNumber%"
-echo 请输入进程号:
-set /p processId=
-TASKKILL /PID %processId% /F
-pause
--- lovelearning/product/setEnvWindow/setJavaHome.bat
+++
@@ -1,6 +0,0 @@
-echo 请输入java安装目录，如：C:\Program Files (x86)\Java\jdk1.6.0_22
-:start
-set /p JAVA_HOME=
-if not exist "%JAVA_HOME%" (echo 你输入路径不存在！！ &goto :start)
-SETX JAVA_HOME "%JAVA_HOME%"
-pause
